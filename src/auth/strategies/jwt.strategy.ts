34b59f0d32f5e3aa32dafe2ee12fb5f25c49de0a
sushilhemrom500530 <sushilhemrom98@gmail.com>
2026-04-06 21:58:57 +0600

    add auth dto, auth guard, and auth strategy wip

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { UsersService } from './../../users/users.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: ConfigService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: string; email: string; role: string }) {
+    const user = await this.usersService.findById(payload.sub);
+    if (!user) throw new UnauthorizedException('User not found');
+    return user; // attaches to req.user
+  }
+}
